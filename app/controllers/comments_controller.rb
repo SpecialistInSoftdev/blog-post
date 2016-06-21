ee4b16bea4d115ca7f4d316c24049d288e031be9
joney000 <let_me_start@yahoo.com>
2016-06-21 15:36:17 +0530

    added validation , flashes , haml

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,12 +4,13 @@ class CommentsController < ApplicationController
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
+
   def destroy
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
+    @comment = Comment.find(params[:id])
     @comment.destroy
-    redirect_to article_path(@article)
+    redirect_to article_path(@comment.article)
   end
+
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
